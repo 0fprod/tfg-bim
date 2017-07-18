32e2d8373515986526da4f4c3e6a2ff8912ee389
franjpr <franpr90@gmail.com>
2017-07-18 17:38:35 +0100

    FavIcon

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 var express    = require('express'),
     path       = require('path'),
     app        = express(),
-    rtIndex    = require('./routes/route_index.js');
+    rtIndex    = require('./routes/route_index.js'),
+    favicon    = require('serve-favicon'),
     helmet     = require('helmet');
 
 app.listen(process.env.PORT || 8080);
 app.set('viewengine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
 app.use('/', rtIndex);
 app.use(helmet());
 
